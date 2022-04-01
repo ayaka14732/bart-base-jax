640996723248c68afcfdc51ea8ac4658430221c8
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-04-01 23:22:02 +0800

    convert chinese parameters

--- finetune.py
+++ finetune.py
@@ -16,12 +16,15 @@ from lib.fwd_transformer import fwd_transformer
 #3. fine-tune params including linear, first layer attention
 #4. fine-tune all params with decayed lr
 
-def recursive_turn_to_jnp(params):  #turn dict of (list of) pytorch tensor into JAX array
-    for k in params:
-        if isinstance(params[k],torch.nn.parameter.Parameter):
-            params[k] = np.asarray(params[k].detach().numpy())
-        else:
-            recursive_turn_to_jnp(params[k])
+
+def load_params():
+    from flax.serialization import msgpack_restore
+    with open('bart_params.dat', 'rb') as f:
+        b = f.read()
+    params = msgpack_restore(b)
+    params = jax.tree_map(np.asarray, params)  # NumPy array to JAX array
+    return params
+
 
 
 # 1.
@@ -34,7 +37,7 @@ w_initializer = jax.nn.initializers.orthogonal()
 b_initializer = jax.nn.initializers.uniform(1/math.sqrt(768))
 linear_params = {'kernel':w_initializer(rand.PRNGKey(42), (768, 768), np.float32),'bias':b_initializer(rand.PRNGKey(42), (768), jnp.float32)}
 
-
+en_params = load_params()
 
 
 
@@ -43,7 +46,7 @@ linear_params = {'kernel':w_initializer(rand.PRNGKey(42), (768, 768), np.float32
 
 # https://github.com/google/jax/issues/9973#issuecomment-1073579382
 
-lm_head = params['embedding']['embedding'].T
+lm_head = en_params['embedding']['embedding'].T
 
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 
