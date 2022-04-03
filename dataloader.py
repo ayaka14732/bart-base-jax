a5ce68da76f430639f6ab8288e8d5863536375ce
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-04-03 12:25:10 +0800

    convert chinese parameters

--- dataloader.py
+++ dataloader.py
@@ -1,10 +1,9 @@
 import math
 from multiprocessing import Pool
-from xmlrpc.client import boolean
 import numpy as np
 from os.path import expanduser, join
 from tqdm import tqdm
-from transformers import BertTokenizer, BartTokenizer
+from transformers import BartTokenizer, BertTokenizer
 
 def chunks(lst, n):
     '''Yield successive n-sized chunks from lst.'''
@@ -13,83 +12,136 @@ def chunks(lst, n):
 
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
-chunksize = 1000
-
+en_tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
 ch_tokenizer = BertTokenizer.from_pretrained('fnlp/bart-base-chinese')
 
-en_tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+max_length_en = 512
+max_length_ch = 256
+chunksize = 1000
 
-def encode_one_batch_ch(sents):
-    assert isinstance(sents, list)
-    assert len(sents) > 1
-    y = ch_tokenizer(sents, return_tensors='np', max_length=256, padding='max_length')
+def encode_en_batch(sents):
+    #assert isinstance(sents, list)
+    #assert len(sents) > 1
+    y = en_tokenizer(sents, return_tensors='np', max_length=max_length_en, padding='max_length')
     data = y.input_ids
     mask = y.attention_mask
     return data, mask
 
-def encode_one_batch_en(sents):
-    assert isinstance(sents, list)
-    assert len(sents) > 1
-    y = en_tokenizer(sents, return_tensors='np', max_length=512, padding='max_length')
+def encode_ch_batch(sents):
+    #assert isinstance(sents, list)
+    #assert len(sents) > 1
+    y = ch_tokenizer(sents, return_tensors='np', max_length=max_length_ch, padding='max_length')
     data = y.input_ids
     mask = y.attention_mask
     return data, mask
 
 
-def process_one_dataset(dataset_filename, language):
-    if language == 'zh':
-        encode_one_batch = encode_one_batch_ch
-        max_length = 256
-    elif language == 'en':
-        encode_one_batch = encode_one_batch_en
-        max_length = 512
-    else:
-        print('unsupported language specification ' + language)
-        exit(0)
+def process_one_dataset(dataset_ch, dataset_en):
+    with open(join(expanduser(f'~/dataset/processed'), dataset_ch), encoding='utf-8') as fch:
+        with open(join(expanduser(f'~/dataset/processed'), dataset_en), encoding='utf-8') as fen:
+            zh = []
+            en = []
 
-
-    with open(join(expanduser(f'~/dataset/processed'), dataset_filename), encoding='utf-8') as f:
-        raw = []
-        for i,line in enumerate(f):
-            line = line.rstrip('\n')
-            # if len(line) <= max_length - 2:  # skip long sentences
-            raw.append(line)
+            for  linech, lineen in zip(fch, fen):
+                linech = linech.rstrip('\n')
+                lineen = lineen.rstrip('\n')
+                if len(linech) <= max_length_ch - 2 and len(lineen.split()) <= max_length_en - 2:  # skip long sentences
+                    zh.append(linech)
+                    en.append(lineen)
 
     with Pool() as p:
-        xs = list(tqdm(p.imap(encode_one_batch,chunks(raw, chunksize)), total=math.ceil(len(raw) / chunksize)))
+        xs = list(tqdm(p.imap(encode_en_batch, chunks(en, chunksize)), total=math.ceil(len(en) / chunksize)))
+        ys = list(tqdm(p.imap(encode_ch_batch, chunks(zh, chunksize)), total=math.ceil(len(zh) / chunksize)))
 
-    data_list = []
-    mask_list = []
+    data_list_en = []
+    mask_list_en = []
+    data_list_ch = []
+    mask_list_ch = []
 
     for data, mask in xs:
-        data_list.append(data)
-        mask_list.append(mask)
+        data_list_en.append(data)
+        mask_list_en.append(mask)
 
-    data_arr = np.vstack(data_list)
-    mask_arr = np.vstack(mask_list)
+    for data, mask in ys:
+        data_list_ch.append(data)
+        mask_list_ch.append(mask)
 
-    return data_arr, mask_arr.astype(boolean)
+    data_arr_en = np.vstack(data_list_en)
+    mask_arr_en = np.vstack(mask_list_en)
+    data_arr_ch = np.vstack(data_list_ch)
+    mask_arr_ch = np.vstack(mask_list_ch)
+
+    return data_arr_ch, mask_arr_ch.astype(np.bool_), data_arr_en, mask_arr_en.astype(np.bool_)
 
 
-# data_arr, mask_arr = process_one_dataset('wikimatrix21zh.txt')
-# #np.savez("wikimatrix21zh.npz", ch_data = data_arr, ch_mask = mask_arr)
-# print("wikimatrix completed")
 
+#ch_tokenizer = BertTokenizer.from_pretrained("fnlp/bart-base-chinese")
+
+data_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch = process_one_dataset('wikimatrix21.zh', 'wikimatrix21.en')
+#np.savez("wikimatrix21zh.npz", ch_data = data_arr_ch, ch_mask = data_arr_ch)
+# print("Done1")
+# del data_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch
 #
-# data_arr, mask_arr = process_one_dataset('UN21.zh')
-# #np.savez("UN21zh.npz", ch_data = data_arr, ch_mask = mask_arr)
-# print("UN completed")
+# data_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch = process_one_dataset('UN21.zh', 'UN21.en')
+# print("Done2")
+# #np.savez("UN21en.npz", ch_data = data_arr, ch_mask = mask_arr)
+# #print("Done22")
+# del data_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch
 #
-#
-#
-# data_arr, mask_arr = process_one_dataset('backtranslatednews.zh')
-# #np.savez("backtranslatednewszh.npz", ch_data = data_arr, ch_mask = mask_arr)
-# print("backtranslated completed")
-# #/home/sarun/dataset/processed/backtranslatednews.en
-#
-# max_length = 128
-#
-# data_arr, mask_arr = process_one_dataset('wikititles21zh.txt')
+# data_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch = process_one_dataset('Wikititles21.zh', 'Wikititles21.en')
+# print("Done3")
 # #np.savez("wikititles21zh.npz", ch_data = data_arr, ch_mask = mask_arr)
-# print("wikititles completed")
+# del data_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch
+#
+# max_length_en = 256
+# max_length_ch = 128
+# ddata_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch = process_one_dataset('backtr.zh', 'backtr.en')
+# #np.savez("backtranslatednewszh.npz", ch_data = data_arr, ch_mask = mask_arr)
+# print("Done4")
+# del data_arr_en, mask_arr_en, data_arr_ch, mask_arr_ch
 
+
+
+#/home/sarun/dataset/processed/backtranslatednews.en
+
+
+'''
+from transformers import BertTokenizer
+import numpy as np
+from tempfile import TemporaryFile
+
+import csv
+
+en_tokenizer = BertTokenizer.from_pretrained("fnlp/bart-base")
+
+with open("/home/sarun/dataset/processed/wikimatrix21en.txt", encoding='utf-8') as file:
+    wikimatrix21en = file.read().splitlines()
+
+batch = en_tokenizer(wikimatrix21en, return_tensors = 'np', max_length = 1024, padding = 'max_length')
+en_data = batch["input_ids"]
+en_mask = batch["attention_mask"]
+np.savez("wikimatrix21en.npz", en_data = en_data, en_mask = en_mask)
+
+with open("/home/sarun/dataset/processed/UN21en.txt", encoding='utf-8') as file:
+    un211en = file.read().splitlines()
+
+batch = en_tokenizer(wikimatrix21en, return_tensors = 'np', max_length = 1024, padding = 'max_length')
+en_data = batch["input_ids"]
+en_mask = batch["attention_mask"]
+np.savez("UN21en.npz", en_data = en_data, en_mask = en_mask)
+
+with open("/home/sarun/dataset/processed/wikititles21en.txt", encoding='utf-8') as file:
+    wikititles21en = file.read().splitlines()
+
+batch = en_tokenizer(wikimatrix21en, return_tensors = 'np', max_length = 128, padding = 'max_length')
+en_data = batch["input_ids"]
+en_mask = batch["attention_mask"]
+np.savez("wikititles21en.npz", en_data = en_data, en_mask = en_mask)
+
+with open("/home/sarun/dataset/processed/backtranslatednews.en", encoding='utf-8') as file:
+    backtren = file.read().splitlines()
+
+batch = en_tokenizer(backtren, return_tensors = 'np', max_length = 1024, padding = 'max_length')
+en_data = batch["input_ids"]
+en_mask = batch["attention_mask"]
+np.savez("backtranslatednews.npz", en_data = en_data, en_mask = en_mask)'''
