bf129c65d6fc81ca34b99dc483a3f4b90b5bc553
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-04-03 16:31:02 +0800

    convert chinese parameters

--- finetune.py
+++ finetune.py
@@ -223,6 +223,9 @@ for _ in tqdm_epoch:
         batch_loss = jax.device_get(jax.tree_map(lambda x: x[0], loss)).item()
         epoch_loss += batch_loss
 
+        if i%10==0:
+            tqdm_epoch.set_postfix({'batch loss': f'{batch_loss:.4f}'})
+
     epoch_loss /= n_batches
     tqdm_epoch.set_postfix({'epoch loss': f'{epoch_loss:.4f}'})
 
