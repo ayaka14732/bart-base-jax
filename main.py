9dcd0c51cf1daf179d515767983534196420d90c
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-04-05 18:58:53 +0800

    change lr

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import jax
 import jax.nn as nn
 import jax.numpy as np
-import numpy as onp
 from transformers import BartTokenizer
 
 from lib.fwd_transformer import fwd_transformer
@@ -28,58 +27,42 @@ tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 
 # sentences = ['Can you see the beautiful flowers <mask> alongside the track?']
 
-# sentences = ['Can you see the beautiful flowers blooming alongside the track?']
-#
-# batch = tokenizer(sentences, return_tensors='jax',padding=True)
+sentences = ['Can you see the beautiful flowers blooming alongside the track?']
 
-# src = batch.input_ids
-# mask_enc_1d = batch.attention_mask.astype(np.bool_)
+batch = tokenizer(sentences, return_tensors='jax',padding=True)
 
-from dataloader import process_one_dataset
-input_ids, mask_enc_1d, decoder_input_ids, mask_dec_1d = process_one_dataset('newscom21.zh', 'newscom21.en',2)
-src = np.hstack((decoder_input_ids[:,1:],[[1*tokenizer.pad_token_id]]*len(input_ids)))
-dst = decoder_input_ids
+src = batch.input_ids
+mask_enc_1d = batch.attention_mask.astype(np.bool_)
 
 i = 1
 # dst = np.zeros((len(sentences), 1), dtype=np.int32)
-# dst = np.asarray(np.asarray([0])+src[:-1],dtype=np.int32)
-# print(tokenizer.batch_decode(dst, skip_special_tokens=False))
+dst = np.asarray(np.asarray([2])+src[:-1],dtype=np.int32)
+print(tokenizer.batch_decode(dst, skip_special_tokens=False))
+
+# while True:
+mask_dec_1d = np.ones((len(sentences), i), dtype=np.bool_)
 
 mask_enc = np.einsum('bi,bj->bij', mask_enc_1d, mask_enc_1d)[:, None]
 mask_dec = np.tril(np.einsum('bi,bj->bij', mask_dec_1d, mask_dec_1d))[:, None]
 mask_dec_enc = np.einsum('bi,bj->bij', mask_dec_1d, mask_enc_1d)[:, None]
 
-labels = src
-
 y = fwd_transformer(params, src, dst, mask_enc, mask_dec, mask_dec_enc)
-a = y @ lm_head
 
+# a = nn.softmax(y @ lm_head)
+# a = np.argmax(a[:, -1], axis=-1)
+
+a = y @ lm_head
 exp_logits = np.exp(a)
 softmax_probs = exp_logits / np.sum(exp_logits, axis=-1, keepdims=True)
-exp_loss = np.take_along_axis(softmax_probs, labels[..., None], axis=-1)
+exp_loss = np.take_along_axis(softmax_probs, src[..., None], axis=-1)
 loss = -np.log(exp_loss)
 print(loss)
 
-a = np.argmax(softmax_probs[:, -1], axis=-1)
-print(tokenizer.batch_decode(src, skip_special_tokens=False))
-print(tokenizer.batch_decode(a, skip_special_tokens=False))
+dst = np.argmax(softmax_probs[:, -1], axis=-1)
+# i += 1
+# dst = np.hstack((dst, a[..., None]))
 
-# while True:
-#     # mask_dec_1d = np.ones((len(sentences), i), dtype=np.bool_)
-#
-#     mask_enc = np.einsum('bi,bj->bij', mask_enc_1d, mask_enc_1d)[:, None]
-#     mask_dec = np.tril(np.einsum('bi,bj->bij', mask_dec_1d, mask_dec_1d))[:, None]
-#     mask_dec_enc = np.einsum('bi,bj->bij', mask_dec_1d, mask_enc_1d)[:, None]
-#
-#     y = fwd_transformer(params, src, dst, mask_enc, mask_dec, mask_dec_enc)
-#
-#     a = nn.softmax(y @ lm_head)
-#     a = np.argmax(a[:, -1], axis=-1)
-#
-#     i += 1
-#     dst = np.hstack((dst, a[..., None]))
-#
-#     if np.all(a == 2):
-#         break
-# print(tokenizer.batch_decode(src, skip_special_tokens=False))
-# print(tokenizer.batch_decode(dst, skip_special_tokens=False))
+# if np.all(a == 2):
+#     break
+print(tokenizer.batch_decode(src, skip_special_tokens=False))
+print(tokenizer.batch_decode(dst, skip_special_tokens=False))
