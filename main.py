0dddafa1d7903f455ab332452882ae4eb0a67d0e
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-04-05 17:00:15 +0800

    change lr

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ mask_enc_1d = batch.attention_mask.astype(np.bool_)
 
 i = 1
 # dst = np.zeros((len(sentences), 1), dtype=np.int32)
-dst = '<s>' + src[:-1]
+dst = np.zeros((1,1)) + src[:-1]
 
 while True:
     mask_dec_1d = np.ones((len(sentences), i), dtype=np.bool_)
