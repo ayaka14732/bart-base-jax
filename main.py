ce8c7eb3a4f9e907caba439b8cad82776a77176d
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-04-05 17:01:27 +0800

    change lr

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ mask_enc_1d = batch.attention_mask.astype(np.bool_)
 
 i = 1
 # dst = np.zeros((len(sentences), 1), dtype=np.int32)
-dst = np.zeros((1,1)) + src[:-1]
+dst = np.zeros((1,1),dtype=np.int32) + src[:-1]
+print(tokenizer.batch_decode(dst, skip_special_tokens=False))
 
 while True:
     mask_dec_1d = np.ones((len(sentences), i), dtype=np.bool_)
