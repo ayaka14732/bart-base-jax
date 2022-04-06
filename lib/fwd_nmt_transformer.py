d7323c5b51d092e71efce3ac04d427ab0a3514d4
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-04-06 05:37:10 +0000

    Update fwd_nmt_transformer.py

--- lib/fwd_nmt_transformer.py
+++ lib/fwd_nmt_transformer.py
@@ -10,27 +10,22 @@ from .fwd_transformer_encoder import fwd_transformer_encoder
 from .fwd_transformer_decoder import fwd_transformer_decoder
 
 def fwd_nmt_transformer(params: dict, src: np.ndarray, dst: np.ndarray, mask_enc: np.ndarray, mask_dec: np.ndarray, mask_dec_enc: np.ndarray, dropout_key: rand.KeyArray=None) -> np.ndarray:
-    # params
-
+    # params_ch
     params_ch = params['ch']
-
     ch_embedding: dict = params_ch['embedding']  # embedding
     ch_encoder_embed_positions: np.ndarray = params_ch['encoder_embed_positions']  # array
     ch_encoder_embed_layer_norm: dict = params_ch['encoder_embed_layer_norm']  # layer norm
     ch_encoder_layers: list = params_ch['encoder_layers']  # list of transformer encoder
 
-
-
+    # params
     embedding: dict = params['embedding']  # embedding
-    # encoder_embed_positions: np.ndarray = params['encoder_embed_positions']  # array
     decoder_embed_positions: np.ndarray = params['decoder_embed_positions']  # array
-    encoder_embed_layer_norm: dict = params['encoder_embed_layer_norm']  # layer norm
     decoder_embed_layer_norm: dict = params['decoder_embed_layer_norm']  # layer norm
     encoder_layers: list = params['encoder_layers']  # list of transformer encoder
     decoder_layers: list = params['decoder_layers']  # list of transformer encoder
 
     if dropout_key is not None:
-        num_keys = 3 + 2*len(encoder_layers) + len(decoder_layers)
+        num_keys = 3 + 2 * len(encoder_layers) + len(decoder_layers)
         keys = iter(rand.split(dropout_key, num=num_keys))
 
     _, width_enc = src.shape
@@ -40,7 +35,7 @@ def fwd_nmt_transformer(params: dict, src: np.ndarray, dst: np.ndarray, mask_enc
     offset = 2
 
     src = fwd_embedding(ch_embedding, src)
-    src = src + ch_encoder_embed_positions[offset:width_enc + offset]
+    src = src + ch_encoder_embed_positions[offset:width_enc+offset]
     src = fwd_layer_norm(ch_encoder_embed_layer_norm, src)
     if dropout_key is not None:
         src = dropout(next(keys), src)
@@ -49,10 +44,6 @@ def fwd_nmt_transformer(params: dict, src: np.ndarray, dst: np.ndarray, mask_enc
             src = fwd_transformer_encoder(encoder_layer, src, mask_enc, dropout_key=next(keys))
         else:
             src = fwd_transformer_encoder(encoder_layer, src, mask_enc)
-
-    # src = fwd_embedding(embedding, src)
-    # src = src + encoder_embed_positions[offset:width_enc+offset]
-    # src = fwd_layer_norm(encoder_embed_layer_norm, src)
 
     if dropout_key is not None:
         src = dropout(next(keys), src)
