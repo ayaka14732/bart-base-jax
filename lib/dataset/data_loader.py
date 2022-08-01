fb436e43ec2a397d55454eef8e9ddd6d2ea7301c
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-08-01 22:13:24 +0800

    Fix bug

--- lib/dataset/data_loader.py
+++ lib/dataset/data_loader.py
@@ -12,17 +12,23 @@ from typing import Any, List, Optional, Tuple
 from .distort_sentence import distort_sentence
 from ..random.wrapper import key2seed, split_key
 
-def load_all_sentences():
+def load_all_sentences(limit: Optional[int]=None):
+    filenames = glob(os.path.expanduser('~/.cache/dump2/*/*'))
+    if not filenames:
+        raise ValueError('Cannot find the dataset in ~/.cache/dump2.')
+    if limit is not None:
+        filenames = filenames[:limit]
+
     all_sentences = []
-    for filename in tqdm(glob(os.path.expanduser('~/.cache/dump2/*/*'))[:10]):
+    for filename in tqdm(filenames):
         with open(filename, encoding='utf-8') as f:
             for line in f:
                 all_sentences.append(line.rstrip('\n'))
     return all_sentences
 
-def chunks(lst: List[Any], chunksize: int) -> List[List[Any]]:
+def chunks(lst: List[Any], chunk_size: int) -> List[List[Any]]:
     '''Yield successive n-sized chunks from lst.'''
-    return [lst[i:i+chunksize] for i in range(0, len(lst), chunksize)]
+    return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
 
 def transform(tokenizer: PreTrainedTokenizer, sentences: List[str], key: rand.KeyArray) -> np.ndarray:
     keys = split_key(key, num=len(sentences))
@@ -42,9 +48,9 @@ def transform_(tokenizer: PreTrainedTokenizer, sentences_key: Tuple[List[str], r
     sentences, key = sentences_key
     return transform(tokenizer, sentences, key)
 
-def producer(queue: multiprocessing.Queue, n_epochs: int, n_workers: int, key: rand.KeyArray, chunksize: int=1024):
+def producer(queue: multiprocessing.Queue, n_epochs: int, n_workers: int, limit: int, chunk_size: int, key: rand.KeyArray):
     print('INFO: Loading sentences...')
-    all_sentences = load_all_sentences()
+    all_sentences = load_all_sentences(limit=limit)
     print(f'INFO: Loaded {len(all_sentences)} sentences.')
 
     key, subkey = split_key(key, num=2)
@@ -53,7 +59,7 @@ def producer(queue: multiprocessing.Queue, n_epochs: int, n_workers: int, key: r
     rng.shuffle(all_sentences)
     print('INFO: Sentence shuffling completed.')
 
-    all_sentences_chunked = chunks(all_sentences, chunksize=chunksize)
+    all_sentences_chunked = chunks(all_sentences, chunk_size=chunk_size)
     n_chunks = len(all_sentences_chunked)
     print(f'INFO: Successfully split into {n_chunks} chunks.')
     queue.put(n_chunks)
@@ -67,7 +73,7 @@ def producer(queue: multiprocessing.Queue, n_epochs: int, n_workers: int, key: r
             for tokenization_result in p.imap(functools.partial(transform_, tokenizer), zip(all_sentences_chunked, subkeys)):
                 queue.put(tokenization_result)
 
-def data_loader(key: rand.KeyArray, n_epochs: int, n_workers: int=Optional[None]):
+def data_loader(key: rand.KeyArray, n_epochs: int, n_workers: Optional[int]=None, queue_size: int=256, limit: Optional[int]=None, chunk_size: int=1024):
     '''
     An on-demand data loader.
 
@@ -80,21 +86,17 @@ def data_loader(key: rand.KeyArray, n_epochs: int, n_workers: int=Optional[None]
     if __name__ == '__main__':
         key = seed2key(seed=42)
         n_epochs = 4
-        n_workers = 32
 
-        data_iter = data_loader(key=key, n_epochs=n_epochs, n_workers=n_workers)
+        data_iter = data_loader(key=key, n_epochs=n_epochs, n_workers=8, limit=8)
 
         for _ in range(n_epochs):
             for i, (src, mask_enc_1d, dst, mask_dec_1d) in enumerate(data_iter()):
                 print(i, tuple(map(lambda x: x.dtype, (src, mask_enc_1d, dst, mask_dec_1d))))
     ```
     '''
-    if n_workers is None:
-        n_workers = os.cpu_count()
-
     ctx = multiprocessing.get_context('spawn')
-    queue = ctx.Queue(maxsize=n_workers)
-    producer_proc = ctx.Process(target=producer, args=(queue, n_epochs, n_workers, key))
+    queue = ctx.Queue(maxsize=queue_size)
+    producer_proc = ctx.Process(target=producer, args=(queue, n_epochs, n_workers, limit, chunk_size, key))
     producer_proc.start()
 
     n_chunks = queue.get()
