5ce7ca8e827061c06e935407e495db8bae70cc5b
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-08-02 12:44:49 +0800

    Training

--- lib/param_utils/init_params.py
+++ lib/param_utils/init_params.py
@@ -4,10 +4,10 @@ from transformers import BartConfig, FlaxBartModel
 from .flax2jax import flax2jax
 from ..random.wrapper import key2seed
 
-_config = BartConfig.from_pretrained('facebook/bart-base')
+config = BartConfig.from_pretrained('facebook/bart-base')
 
 def init_params(key: rand.KeyArray) -> dict:
     seed = key2seed(key)
-    model_flax = FlaxBartModel(config=_config, seed=seed)
+    model_flax = FlaxBartModel(config=config, seed=seed)
     model_jax = flax2jax(model_flax.params)
     return model_jax
