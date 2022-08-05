b5f45ff48963a6f3aeabeb4953c44a51063c9dc2
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-08-05 12:37:17 +0800

    Revert "Revert "Training""
    This reverts commit bed1d9693e87f6caed8ed87aafd095124a3bbe71.

--- train.py
+++ train.py
@@ -1,52 +1,51 @@
 import functools
 import jax
 import jax.numpy as np
-import jax.random as rand
 import numpy as onp
 import optax
 import time
-from transformers import BartTokenizer
 import wandb
 
-from lib import fwd_transformer
-# from lib.param_utils.load_params import load_params
+from lib.dataset.data_loader import data_loader
+from lib.model.fwd_transformer import fwd_transformer
 from lib.param_utils.init_params import init_params
+from lib.param_utils.save_params import save_params
+from lib.random.wrapper import seed2key, split_key
+from lib.training.cross_entropy_loss import cross_entropy_loss
 
-# Procedure:
-# 1. load a pretrained BART-base-chinese encoder
-# 2. adding a linear layer to 1, substitute the embedding part of pretrained BART-base-english
-# 3. fine-tune params including linear, first layer attention
-# 4. fine-tune all params with decayed lr
+# hyperparameters
 
-devices = jax.local_devices()
-n_devices = jax.local_device_count()
+devices = jax.devices()
+n_devices = jax.device_count()
 assert n_devices == 8
 
-n_epoch = 1
+n_epochs = 1
 batch_size = 16 * n_devices  # 28 * n_devices
 learning_rate = 0.023
 
-en_tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+def get_tokenizer_info():
+    from transformers import BartTokenizer
+
+    en_tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+
+    vocab_size = en_tokenizer.vocab_size
+    pad_token_id = en_tokenizer.pad_token_id
+
+    return vocab_size, pad_token_id
+
+vocab_size, pad_token_id = get_tokenizer_info()
 
 wandb.init(project='bart-pretraining', config={
-    'n_epoch': n_epoch,
+    'n_devices': n_devices,
+    'n_epochs': n_epochs,
     'batch_size': batch_size,
     'learning_rate': learning_rate,
-    'extra_description': 'using sgd optimizer; trained on wikimatrix21; 13th revision of freezing',
+    'vocab_size': vocab_size,
+    'pad_token_id': pad_token_id,
 })
 
-def cross_entropy_loss(logits, labels, mask):
-    labels_onehot = jax.nn.one_hot(labels, num_classes=en_tokenizer.vocab_size)
-    loss = optax.softmax_cross_entropy(logits=logits, labels=labels_onehot)
-    loss *= mask
-    return np.sum(loss)
-
-# 1. load params
-
-params = init_params()
 
 optimizer = optax.adam(learning_rate=learning_rate)
-opt_state = optimizer.init(params)
 
 @jax.jit
 @jax.value_and_grad
@@ -54,7 +53,7 @@ def train_forward(params, src, dst, mask_enc, mask_dec, mask_dec_enc, labels, dr
     outputs = fwd_transformer(params, src, dst, mask_enc, mask_dec, mask_dec_enc, dropout_key=dropout_key)
     lm_head = params['embedding']['embedding'].T
     logits = outputs @ lm_head
-    loss = cross_entropy_loss(logits, labels, mask=mask_dec) / len(labels)
+    loss = cross_entropy_loss(logits, labels, mask=mask_dec, num_classes=vocab_size) / len(labels)
     return loss
 
 @functools.partial(jax.pmap, axis_name='num_devices')
@@ -69,26 +68,9 @@ def train_step(params, opt_state, src, dst, mask_enc, mask_dec, mask_dec_enc, la
 
     return params, opt_state, loss
 
-@jax.jit
-def eval_forward(params, src, dst, mask_enc, mask_dec, mask_dec_enc, labels):
-    outputs = fwd_transformer(params, src, dst, mask_enc, mask_dec, mask_dec_enc)
-    lm_head = params['embedding']['embedding'].T
-    logits = outputs @ lm_head
-    loss = cross_entropy_loss(logits, labels, mask=mask_dec) / len(labels)
-    return loss
-
-@functools.partial(jax.pmap, axis_name='num_devices')
-def eval_step(params, src, dst, mask_enc, mask_dec, mask_dec_enc, labels):
-    loss = eval_forward(params, src, dst, mask_enc, mask_dec, mask_dec_enc, labels)
-    loss = jax.lax.pmean(loss, axis_name='num_devices')
-    return loss
-
 def device_split(arr):
     '''Splits the first axis of `arr` evenly across the number of devices.'''
     return arr.reshape(n_devices, arr.shape[0] // n_devices, *arr.shape[1:])
-
-def do_on_cpu(f):
-    return jax.jit(f, backend='cpu')
 
 def mask_1d_to_2d(mask_enc_1d, mask_dec_1d):
     mask_enc = device_split(np.einsum('bi,bj->bij', mask_enc_1d, mask_enc_1d)[:, None])
@@ -96,67 +78,69 @@ def mask_1d_to_2d(mask_enc_1d, mask_dec_1d):
     mask_dec_enc = device_split(np.einsum('bi,bj->bij', mask_dec_1d, mask_enc_1d)[:, None])
     return mask_enc, mask_dec, mask_dec_enc
 
-replicated_params = jax.device_put_replicated(params, devices)
-replicated_opt_state = jax.device_put_replicated(opt_state, devices)
-
-def save_ckpt():
+def save_checkpoint(replicated_params) -> None:
     params = jax.tree_map(lambda x: x[0], replicated_params)
     filename = f'{wandb.run.name}.dat'
     save_params(params, filename)
 
-key = rand.PRNGKey(42)
-n_sents = len(input_ids)
+# TODO:
+# `data_loader` should yield
+# src, dst, mask_enc, mask_dec, mask_dec_enc, labels
+#
+# should be able to close the child processes (early stopping)
 
-permute = do_on_cpu(lambda key: rand.permutation(key, n_sents))
+def main():
+    params = init_params()
 
-# jax.profiler.start_trace(log_dir='/tmp/jax-profiler')
+    opt_state = optimizer.init(params)
 
-for _ in range(1, n_epoch + 1):
-    epoch_loss = 0.
+    key = seed2key(seed=42)
 
-    n_batches = n_sents // batch_size
-    key, subkey = rand.split(key)
-    shuffled_indices = onp.asarray(permute(subkey))
+    data_iter = data_loader(key=key, n_epochs=n_epochs, n_workers=8, limit=8)
 
-    for i in range(n_batches):
-        start_time = time.time()
+    replicated_params = jax.device_put_replicated(params, devices)
+    replicated_opt_state = jax.device_put_replicated(opt_state, devices)
 
-        batch = shuffled_indices[i*batch_size:(i+1)*batch_size]
+    for _ in range(n_epochs):
 
-        src = input_ids[batch]
-        dst = decoder_input_ids[batch]
-        labels = onp.hstack((dst[:, 1:], np.ones((len(batch), 1), dtype=np.int32) * en_tokenizer.pad_token_id))
+        epoch_loss = 0.
 
-        src = device_split(src)
-        dst = device_split(dst)
-        labels = device_split(labels)
+        for i, (src, mask_enc_1d, dst, mask_dec_1d) in enumerate(data_iter()):
+            start_time = time.time()
 
-        batch_mask_enc_1d = mask_enc_1d[batch]
-        batch_mask_dec_1d = mask_dec_1d[batch]
+            labels = onp.hstack((dst[:, 1:], np.ones((len(batch), 1), dtype=np.int32) * pad_token_id))
 
-        mask_enc = np.einsum('bi,bj->bij', batch_mask_enc_1d, batch_mask_enc_1d)[:, None]
-        mask_dec = np.tril(np.einsum('bi,bj->bij', batch_mask_dec_1d, batch_mask_dec_1d))[:, None]
-        mask_dec_enc = np.einsum('bi,bj->bij', batch_mask_dec_1d, batch_mask_enc_1d)[:, None]
+            src = device_split(src)
+            dst = device_split(dst)
+            labels = device_split(labels)
 
-        mask_enc = device_split(mask_enc)
-        mask_dec = device_split(mask_dec)
-        mask_dec_enc = device_split(mask_dec_enc)
+            batch_mask_enc_1d = mask_enc_1d[batch]
+            batch_mask_dec_1d = mask_dec_1d[batch]
 
-        key, subkey = (lambda keys: (keys[0], keys[1:]))(rand.split(key, num=9))
+            mask_enc = np.einsum('bi,bj->bij', batch_mask_enc_1d, batch_mask_enc_1d)[:, None]
+            mask_dec = np.tril(np.einsum('bi,bj->bij', batch_mask_dec_1d, batch_mask_dec_1d))[:, None]
+            mask_dec_enc = np.einsum('bi,bj->bij', batch_mask_dec_1d, batch_mask_enc_1d)[:, None]
 
-        replicated_params, replicated_opt_state, replicated_loss = train_step(replicated_params, replicated_opt_state, src, dst, mask_enc, mask_dec, mask_dec_enc, labels, dropout_key=subkey)
+            mask_enc = device_split(mask_enc)
+            mask_dec = device_split(mask_dec)
+            mask_dec_enc = device_split(mask_dec_enc)
 
-        batch_loss = replicated_loss[0].item()
-        assert not np.isnan(batch_loss)
-        epoch_loss += batch_loss
+            key, subkey = split_key(key, nums=9)
 
-        elapsed_time = time.time() - start_time
+            replicated_params, replicated_opt_state, replicated_loss = train_step(replicated_params, replicated_opt_state, src, dst, mask_enc, mask_dec, mask_dec_enc, labels, dropout_key=subkey)
 
-        wandb.log({'batch loss': batch_loss, 'time': elapsed_time})
+            batch_loss = replicated_loss[0].item()
+            assert not np.isnan(batch_loss)
+            epoch_loss += batch_loss
 
-    epoch_loss /= n_batches
-    wandb.log({'epoch loss': epoch_loss})
+            elapsed_time = time.time() - start_time
 
-    save_ckpt()
+            wandb.log({'batch loss': batch_loss, 'time': elapsed_time})
 
-# jax.profiler.stop_trace()
+        epoch_loss /= n_batches
+        wandb.log({'epoch loss': epoch_loss})
+
+        save_checkpoint(replicated_params)
+
+if __name__ == '__main__':
+    main()
