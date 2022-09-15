186cba0cff491155310b93d728ef4849ed7ea307
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-09-15 21:07:59 +0800

    Finish tokeniser

--- lib/twblg/CharBasedTokeniser.py
+++ lib/twblg/CharBasedTokeniser.py
@@ -1,40 +1,79 @@
-class CharBasedTokeniser:
-    def __init__(self, vocab: list[str]) -> None:
+from os import makedirs
+from os.path import join
+from transformers import AddedToken, PreTrainedTokenizer
+from typing import Union, Optional
+
+class CharBasedTokenizer(PreTrainedTokenizer):
+
+    model_input_names = ['input_ids', 'attention_mask']
+
+    def __init__(self, vocab: Union[list[str], str]) -> None:
+        if isinstance(vocab, str):
+            with open(vocab, encoding='utf-8') as f:
+                vocab = [line.rstrip('\n') for line in f]
+
+        super().__init__(
+            pad_token=AddedToken('[PAD]'),
+            unk_token=AddedToken('[UNK]'),
+            bos_token=AddedToken('[BOS]'),
+            eos_token=AddedToken('[EOS]'),
+            mask_token=AddedToken('[MSK]'),
+        )
+
+        assert vocab[0] == '[PAD]'
+        assert vocab[1] == '[UNK]'
+        assert vocab[2] == '[BOS]'
+        assert vocab[3] == '[EOS]'
+        assert vocab[4] == '[MSK]'
+
+        self.special_tokens_encoder = {
+            self.pad_token: 0,
+            self.unk_token: 1,
+            self.bos_token: 2,
+            self.eos_token: 3,
+            self.mask_token: 4,
+        }
+        self._num_special_tokens = len(self.special_tokens_encoder)
+        self.special_tokens_decoder = {v: k for k, v in self.special_tokens_encoder.items()}
+
         self.id2ch = vocab
         self.ch2id = {c: i for i, c in enumerate(vocab)}
 
-        self.vocab_size = len(vocab)
+        assert self.pad_token_id == 0
+        assert self.unk_token_id == 1
+        assert self.bos_token_id == 2
+        assert self.eos_token_id == 3
+        assert self.mask_token_id == 4
 
-        self.pad_token = self.ch2id['[PAD]']
-        self.unk_token = self.ch2id['[UNK]']
-        self.cls_token = self.ch2id['[CLS]']
-        self.sep_token = self.ch2id['[SEP]']
-        self.msk_token = self.ch2id['[MASK]']
+    @property
+    def vocab_size(self):
+        return len(self.id2ch)
 
-    @classmethod
-    def from_vocab_file(cls, path: str):
-        vocab = []
-        with open(path, encoding='utf-8') as f:
-            for line in f:
-                c = line.rstrip('\n')
-                vocab.append(c)
-        return cls(vocab)
+    def build_inputs_with_special_tokens(self, token_ids_0: list[int], token_ids_1) -> list[int]:
+        assert token_ids_1 is None
+        return [self.bos_token_id, *token_ids_0, self.eos_token_id]
 
-    def tokenise_sentence(self, s: str) -> list[str]:
-        return [
-            self.cls_token,
-            *(self.ch2id.get(c, self.unk_token) for c in s),
-            self.sep_token,
-        ]
+    def _tokenize(self, text: str) -> list[str]:
+        return list(text)
 
-    def detokenise_sentence(self, l: list) -> list[str]:
-        while l and l[-1] == self.pad_token:
-            l.pop()
-        if l and l[0] == self.cls_token:
-            l = l[1:]
-        if l and l[-1] == self.sep_token:
-            l.pop()
-        return ''.join(self.id2ch[i] for i in l)
+    def _convert_token_to_id(self, token):
+        unk_token_id = self.ch2id[self.unk_token]
+        return self.ch2id.get(token, unk_token_id)
 
-    def __call__(self, s: str):
-        pass
+    def _convert_id_to_token(self, index):
+        return self.id2ch[index]
+
+    def convert_tokens_to_string(self, tokens):
+        return ''.join(tokens)
+
+    def save_vocabulary(self, save_directory: Optional[str]=None, filename_prefix: Optional[str]=None) -> tuple[str]:
+        filename = 'vocab.txt'
+        if filename_prefix is not None:
+            filename = filename_prefix + filename
+        if save_directory is not None:
+            makedirs(save_directory, exist_ok=True)
+            filename = join(save_directory, filename)
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            for w in self.id2ch:
+                print(w, file=f)
---
+++ test/twblg/CharBasedTokeniser.py
@@ -0,0 +1,32 @@
+from pathlib import Path; import sys; sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+
+from os.path import join
+import tempfile
+
+from lib.twblg.CharBasedTokeniser import CharBasedTokenizer
+
+vocab = [
+    '[PAD]',
+    '[UNK]',
+    '[BOS]',
+    '[EOS]',
+    '[MSK]',
+    '五',
+    '六',
+    '七',
+    '八',
+    '九',
+]
+
+tokeniser = CharBasedTokenizer(vocab=vocab)
+with tempfile.TemporaryDirectory() as tmpdirname:
+    tokeniser.save_vocabulary(save_directory=tmpdirname, filename_prefix='prefix_')
+    del tokeniser
+    vocab_file = join(tmpdirname, 'prefix_vocab.txt')
+    tokeniser = CharBasedTokenizer(vocab=vocab_file)
+
+inputs = tokeniser(['五六', '七八九十'], return_tensors='np', max_length=8, padding='max_length', truncation=True)
+inputs.input_ids[0].tolist()[:4] == [2, 5, 6, 3]
+inputs.input_ids[1].tolist()[:6] == [2, 7, 8, 9, 1, 3]
+inputs.attention_mask[0].tolist() == [1, 1, 1, 1, 0, 0, 0, 0]
+inputs.attention_mask[1].tolist() == [1, 1, 1, 1, 1, 1, 0, 0]
