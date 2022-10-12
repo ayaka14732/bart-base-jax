ec1fd34672f713b0c097497fca64ceb018199c91
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-10-12 22:09:29 +0800

    Fix bug

--- lib/dataloader/device_split.py
+++ lib/dataloader/device_split.py
@@ -7,10 +7,10 @@ from typeguard import typechecked as typechecker
 @typechecker
 def device_split(a: S[onp.ndarray, '...']) -> S[Array, '...']:
     local_devices = jax.local_devices()
-    n_local_devices = jax.device_count()
+    n_local_devices = jax.local_device_count()
 
     '''Splits the first axis of `a` evenly across the number of devices.'''
     batch_size, *shapes = a.shape
-    a = a.reshape(local_devices, batch_size // n_local_devices, *shapes)
+    a = a.reshape(n_local_devices, batch_size // n_local_devices, *shapes)
     b = jax.device_put_sharded(tuple(a), devices=local_devices)
     return b
