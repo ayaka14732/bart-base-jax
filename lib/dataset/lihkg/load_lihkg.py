58f603a4be44c70eda266451cd0f116a9756fdce
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-10-25 15:04:05 +0800

    Use deduped dataset

--- lib/dataset/lihkg/load_lihkg.py
+++ lib/dataset/lihkg/load_lihkg.py
@@ -1,7 +1,7 @@
 from os.path import expanduser
 
 def load_lihkg() -> list[str]:
-    filename = expanduser('~/lihkg-1-2850000-processed.csv')
+    filename = expanduser('~/lihkg-1-2850000-processed-dedup.csv')
     sentences = []
     with open(filename, encoding='utf-8') as f:
         for line in f:
