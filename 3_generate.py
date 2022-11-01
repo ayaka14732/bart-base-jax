f44b4a58c4b491d38ab1058b8a23abde821410d6
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-11-01 10:07:19 +0800

    Generate script

--- 3_generate.py
+++ 3_generate.py
@@ -6,7 +6,7 @@ from lib.Generator import Generator
 from lib.param_utils.load_params import load_params
 from lib.en_kfw_nmt.fwd_transformer_encoder_part import fwd_transformer_encoder_part
 
-params = load_params('frightful-wight-24-34.dat')
+params = load_params('occult-whisper-25-99.dat')
 params = jax.tree_map(np.asarray, params)
 
 tokenizer_en = BartTokenizer.from_pretrained('facebook/bart-base')
@@ -17,7 +17,14 @@ generator = Generator({'embedding': params['decoder_embedding'], **params}, conf
 
 # generate
 
-sentences = ['Fire!', 'Are you feeling unwell?', 'How long have you waited?']
+sentences = [
+    'Fire!',
+    'Are you feeling unwell?',
+    'How long have you waited?',
+    'muscular dystrophy',
+    'You guys have no discipline, how can you be part of the disciplined services?',
+    'Set A for takeaway.',
+]
 inputs = tokenizer_en(sentences, return_tensors='jax', max_length=20, padding='max_length', truncation=True)
 src = inputs.input_ids.astype(np.uint16)
 mask_enc_1d = inputs.attention_mask.astype(np.bool_)
