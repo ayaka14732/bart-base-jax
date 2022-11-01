0b117f267ef0922bf24c1df2fc718967799b9f91
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-11-01 16:14:57 +0800

    lr

--- 2_finetune.py
+++ 2_finetune.py
@@ -97,7 +97,7 @@ def main():
     optimizer_scheme = {
         'train': optax.chain(
             optax.adaptive_grad_clip(0.1),
-            optax.sgd(learning_rate=0.05),
+            optax.sgd(learning_rate=0.15),
         ),
         # 'train': optax.adamw(learning_rate=5e-4),
         'freeze': optax.set_to_zero(),
