1c8a7b8f547c05daac797b352ac748221810b6d4
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-11-03 00:25:29 +0800

    Fix bug

--- lib/en_kfw_nmt/load_sentences.py
+++ lib/en_kfw_nmt/load_sentences.py
@@ -6,10 +6,10 @@ def load_sentences(split=Literal['train', 'dev', 'test']) -> list[tuple[str, str
         raise ValueError("`split` should be one of ('train', 'dev', 'test')")
 
     if split == 'train':
-        with fileinput.input(files=('{split}.en.txt', '../abc-cantonese-parallel-corpus/en.txt'), encoding='utf-8') as f:
+        with fileinput.input(files=(f'{split}.en.txt', '../abc-cantonese-parallel-corpus/en.txt'), encoding='utf-8') as f:
             en = [line.rstrip('\n') for line in f]
 
-        with fileinput.input(files=('{split}.yue.txt', '../abc-cantonese-parallel-corpus/yue.txt'), encoding='utf-8') as f:
+        with fileinput.input(files=(f'{split}.yue.txt', '../abc-cantonese-parallel-corpus/yue.txt'), encoding='utf-8') as f:
             yue = [line.rstrip('\n') for line in f]
 
     else:
