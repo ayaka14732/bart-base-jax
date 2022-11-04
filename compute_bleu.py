77adf6fa69c38b2e222bd6eb0d7a61e9c9afde3c
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-11-04 21:55:06 +0800

    BLEU

---
+++ compute_bleu.py
@@ -0,0 +1,22 @@
+from datasets import load_metric
+import fire
+from transformers import BertTokenizer
+
+bleu = load_metric('bleu')
+tokenizer_yue = BertTokenizer.from_pretrained('Ayaka/bart-base-cantonese')
+
+def compute_bleu(predictions_file: str, references_file: str='../wordshk-parallel-corpus/plus15/test.yue.txt') -> None:
+    with open(predictions_file, encoding='utf-8') as f:
+        predictions = [line.rstrip('\n') for line in f]
+    with open(references_file, encoding='utf-8') as f:
+        references = [line.rstrip('\n') for line in f]
+
+    predictions = [tokenizer_yue.tokenize(prediction) for prediction in predictions]
+    references = [[tokenizer_yue.tokenize(reference)] for reference in references]
+
+    results = bleu.compute(predictions=predictions, references=references)
+    for k, v in results.items():
+        print(k, ': ', v, sep='')
+
+if __name__ == '__main__':
+    fire.Fire(compute_bleu)
