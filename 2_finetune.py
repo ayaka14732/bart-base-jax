f06c2bf29095f6cd89478dcf71dfc2951f739c44
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-11-05 09:36:58 +0800

    lr

--- 2_finetune.py
+++ 2_finetune.py
@@ -82,7 +82,7 @@ def main():
     params = load_params('serene-terrain-53.dat')
     params = jax.tree_map(np.asarray, params)
 
-    base_learning_rate = 0.000017
+    base_learning_rate = 0.000005
     learning_rates = (
         base_learning_rate * 0.35,
         base_learning_rate * 0.5,
@@ -92,7 +92,7 @@ def main():
     schedules = [
         optax.join_schedules((
             optax.linear_schedule(0, learning_rate, 50),
-            optax.linear_schedule(learning_rate, learning_rate * 0.35, 12238),
+            optax.linear_schedule(learning_rate, learning_rate * 0.005, 12238),
         ), (50,))
         for learning_rate in learning_rates
     ]
