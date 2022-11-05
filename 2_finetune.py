b785767850bffde283478c8c61db58f87c0ededf
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-11-05 21:34:27 +0800

    Fix bug

--- 2_finetune.py
+++ 2_finetune.py
@@ -61,9 +61,9 @@ def main():
     local_devices = jax.local_devices()
     n_local_devices = jax.local_device_count()
 
-    n_epochs = 12
+    n_epochs = 8
 
-    batch_size_per_device_train = 8
+    batch_size_per_device_train = 4
     batch_size_per_device_dev = 187
 
     key = seed2key(seed=42 + process_index)
